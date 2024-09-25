174a08ddbc4ac8988afbd493194575ebc33beebb
DanleyFernando <160700716+DanleyFernando@users.noreply.github.com>
2024-09-25 00:40:51 -0300

    euodeioonetbesn

--- src/floricultura/LoadLogin.java
+++ src/floricultura/LoadLogin.java
@@ -146,7 +146,7 @@ public class LoadLogin extends javax.swing.JFrame {
         }
     }
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+        /* Set the Nimbus look and feel /
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
